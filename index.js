afd6d9e1154b66aca812acb4da2bb244a0b266bf
DRiFT-kpop <nzxbns8smy@privaterelay.appleid.com>
2026-04-20 16:17:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,12 @@ const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers
+    GatewayIntentBits.MessageContent
   ]
 });
 
 client.once("ready", () => {
   console.log("DR|FT BOT ONLINE");
-});
-
-client.on("messageCreate", message => {
-  if (message.author.bot) return;
-
-  if (message.content.toLowerCase().includes("drift")) {
-    message.reply("signal received.");
-  }
 });
 
 client.login(process.env.TOKEN);
